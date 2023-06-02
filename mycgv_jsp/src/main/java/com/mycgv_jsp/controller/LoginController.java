1e44ca98edf53237762f14eecd981a101109a364
Finnyam <finnyam808@gmail.com>
2023-06-02 12:43:49 +0900

    login session / logout

--- mycgv_jsp/src/main/java/com/mycgv_jsp/controller/LoginController.java
+++ mycgv_jsp/src/main/java/com/mycgv_jsp/controller/LoginController.java
@@ -1,5 +1,7 @@
 package com.mycgv_jsp.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +18,21 @@ public class LoginController {
 	private MemberService memberService;
 	
 	/**
+	 * logout.do - 로그아웃
+	 */
+	@RequestMapping(value="/logout.do", method=RequestMethod.GET)
+	public ModelAndView logout(HttpSession session) {
+		ModelAndView model = new ModelAndView();
+		String sid = (String)session.getAttribute("sid");
+		if(sid != null) {
+			session.invalidate();
+			model.addObject("logout_result","ok");
+		}
+		model.setViewName("index");
+		
+		return model;
+	}
+	/**
 	 * login_fail.do - 로그인 실패
 	 */
 	@RequestMapping(value="/login_fail.do", method=RequestMethod.GET)
@@ -27,13 +44,14 @@ public class LoginController {
 	 * login_proc.do - 로그인 처리
 	 */
 	@RequestMapping(value="/login_proc.do", method=RequestMethod.POST)
-	public ModelAndView login_proc(MemberVo memberVo) {
+	public ModelAndView login_proc(MemberVo memberVo, HttpSession session) {
 		ModelAndView model = new ModelAndView();
 		int result = memberService.getLoginResult(memberVo);
 		
 		if(result == 1) {
 			//index 이동
 			//viewName = "index";  viewResolver를 호출--> index.jsp: header.do,footer.do 호출안됨
+			session.setAttribute("sid", memberVo.getId());
 			model.addObject("login_result", "ok");
 			model.setViewName("index");  //sendRedirect 
 		}else {
